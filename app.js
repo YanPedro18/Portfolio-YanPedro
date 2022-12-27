1f3cb27225b66cd0ebb8c1c72730352a0fdbdef9
Yan Pedro <68964255+YanPedro18@users.noreply.github.com>
2022-12-27 11:16:21 -0300

    New

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-            new TypeIt(".animated", {
-                speed: 300,
-                strings: "Yan Pedro"
-            }).go()
+    new TypeIt(".animated", {
+        speed: 300,
+        strings: "Yan Pedro"
+    }).go()
 })
 
 var speed = {
-    distance: '-50px',
-    duration: 1400,
-    delay: 400
+distance: '-50px',
+duration: 1400,
+delay: 400
 };
 
 var speedmin = {
-    distance: '-10px',
-    duration: 1400,
-    delay: 400
+distance: '-10px',
+duration: 1400,
+delay: 400
 };
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.text-1 > p, .text-1 > h3, .text-1 > h1, .foto, .skills, .sect-04 > h1, .sect-05 > h1, .btn-hover, .sect-05-flex, .card-contact, h6, .row, aside, list-contact', speed ,{delay: 200, origin:'top',});
+sr.reveal('.text-1 > p, .text-1 > h3, .text-1 > h1, .foto, .skills, .sect-04 > h1, .sect-05 > h1, .btn-hover, .sect-05-flex, .card-contact, h6, .row, aside, list-contact > a', speed ,{delay: 200, origin:'top',});
 
 sr.reveal('.sect-flex-1 > img',speed,{
-    duration: 2000,
+duration: 2000,
 });
 
 sr.reveal('.sect-03 h1, p',speedmin,{
-    duration: 1000,
+duration: 1000,
 });
 
 const btnMobile = document.getElementById('btn-mobile');
 
 function menuAct (event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+if (event.type === 'touchstart') event.preventDefault();
+const nav = document.getElementById('nav');
+nav.classList.toggle('active');
 }
 btnMobile.addEventListener('click', menuAct);
 btnMobile.addEventListener('touchstart', menuAct);
